73e6cae1de5e9d5f2b2f6c431b2ec3d3b04341d2
sergio1221 <sergio.pinilos.dev@gmail.com>
2021-10-09 20:50:27 +0300

    missing file

---
+++ apps/fithm-gateway/apps/account/lib/parser.py
@@ -0,0 +1,21 @@
+from flask_restx import reqparse
+from flask import Request
+
+
+class AccountParser:
+    '''Account endpoints arguments parser'''
+
+    def __init__(self):
+        self.create = None
+        self.list = None
+        self.update = None
+
+
+    def parse_create(self, req: Request) -> dict:
+        if not self.create:
+            self.create = reqparse.RequestParser()
+            self.create.add_argument('broker_name', required=True, type=str, location='json')
+            self.create.add_argument('account_number', required=True, type=int, location='json')
+            self.create.add_argument('portfolio_id', type=str, location='json')
+
+        return self.create.parse_args(req)
