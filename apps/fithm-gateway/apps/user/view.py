52d454b6dfce93d3a97ff7aab49fe98f507836b5
sergio1221 <sergio.pinilos.dev@gmail.com>
2021-11-11 10:48:52 +0300

    update profile endpoint fixed

--- apps/fithm-gateway/apps/user/view.py
+++ apps/fithm-gateway/apps/user/view.py
@@ -1,10 +1,16 @@
-from flask import g
+from flask import g, current_app, abort
 from libs.database import db_session
+from libs.depends.entry import container
+from apps.auth.lib.auth.authenticator import Authenticator
 
 from apps.models import User
 
 
 class UserView:
+
+    def __init__(self):
+        self.authenticator: Authenticator = container.get(Authenticator)
+
 
     def get(self):
         '''Get the user'''
@@ -17,9 +23,23 @@ class UserView:
         '''Update user'''
 
         user: User = g.user
+        password = None
+
+        current_app.logger.debug(f'params: {param}, {user.password}')
+        if 'old_password' in param:
+            if not self.authenticator.verify_password(param['old_password'], user.password):
+                current_app.logger.debug('password is incorrect')
+                abort(401, 'password is incorrect')
+
+            password = param['new_password']
+            del param['old_password']
+            del param['new_password']
 
         for key in param:
             setattr(user, key, param[key])
+
+        if password is not None:
+            user.password = self.authenticator.hash_password(password)
 
         db_session.commit()
         return user.as_dict()
